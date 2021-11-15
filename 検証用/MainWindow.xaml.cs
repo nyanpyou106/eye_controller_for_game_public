1cd74d09fb81a390422c935c14fd0ce7cf007979
nyanpyou106 <dummy_address>
2021-11-15 14:36:27 +0900

    視線追従、描画、クリックまで実装

--- 検証用/MainWindow.xaml.cs
+++ 検証用/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using Tobii.StreamEngine;
 using System.Diagnostics;
+using System.Threading;
 
 namespace 検証用
 {
@@ -32,9 +33,15 @@ namespace 検証用
         }
 
         // フラグ等の初期化
-        public static double Dpi_Factor = 10;
+        public static double Dpi_Factor = 1;
         public string SingleOrDouble = "Single";
         OverLay overlay = new OverLay();
+        public static Win32API.POINT gazingpoint = new Win32API.POINT();
+        public const int GazingCircleAreaWidth_init = 100;
+        public const int GazingCircleAreaHeight_init = 100;
+        public int GazingCircleAreaWidth = 100;
+        public int GazingCircleAreaHeight = 100;
+        public int NeedTimeToGaze = 3000; // milliseconds
 
         public void GetDpiFactorAndShowOverLay(object sender, RoutedEventArgs e)
         {
@@ -129,14 +136,11 @@ namespace 検証用
 
             // マウス関連関数
 
-            public static int[] GetCurrentMouseCoordinate()
+            public static POINT GetCurrentMouseCoordinate()
             {
                 Win32API.POINT CURRENTCOORDINATE;
                 var retval = Win32API.GetCursorPos(out CURRENTCOORDINATE);
-                int[] CurrentCoordinate = new int[2];
-                CurrentCoordinate[0] = CURRENTCOORDINATE.x;
-                CurrentCoordinate[1] = CURRENTCOORDINATE.y;
-                return CurrentCoordinate;
+                return CURRENTCOORDINATE;
             }
 
             public static void MouseLeftClick(int X, int Y, string SingleOrDouble)
@@ -145,6 +149,7 @@ namespace 検証用
                 MI_LEFTCLICK.dx = X;
                 MI_LEFTCLICK.dy = Y;
                 MI_LEFTCLICK.mouseData = 0;
+                // マウス動作をビット和で設定
                 MI_LEFTCLICK.dwFlags = Win32API.dwFlags_values.MOUSEEVENTF_LEFTDOWN | Win32API.dwFlags_values.MOUSEEVENTF_LEFTUP;
                 MI_LEFTCLICK.time = 0;
                 MI_LEFTCLICK.dwExtraInfo = UIntPtr.Zero;
@@ -164,6 +169,12 @@ namespace 検証用
             }
         }
 
+        public double CalculateDistance(Win32API.POINT point1, Win32API.POINT point2)
+        {
+            double distance = Math.Pow((Math.Pow(point1.x - point2.x, 2) + Math.Pow(point1.y - point2.y, 2)), 0.5);
+            return distance;
+        }
+
         public void Move_Cursor_Smoothly()
         {
             // クリック地点から対象地点(X,Y)まで滑らかにカーソルを動かす
@@ -171,19 +182,18 @@ namespace 検証用
             // 最終目的地からXかYが数pixelズレてしまう上に、現状それを無理やり直しているためカクっと動いてしまう。
 
             // 現在地と目的地の座標を取得
-            int[] CurrentCoordinate = new int[2];
-            CurrentCoordinate = Win32API.GetCurrentMouseCoordinate();
-            int Current_X = CurrentCoordinate[0];
-            int Current_Y = CurrentCoordinate[1];
-            int Destination_X = Int32.Parse(x_coordinate.Text);
-            int Destination_Y = Int32.Parse(y_coordinate.Text);
+            Win32API.POINT currentpoint = new Win32API.POINT();
+            currentpoint = Win32API.GetCurrentMouseCoordinate();
+            Win32API.POINT destinationpoint = new Win32API.POINT();
+            destinationpoint.x = Int32.Parse(x_coordinate.Text);
+            destinationpoint.y = Int32.Parse(y_coordinate.Text);
 
             // 現在地と目的地の0.15倍の距離だけカーソルを動かしてそれを目的地に着くまで繰り返す
             // 移動のパラメータは実際に試してみていい感じだったもの
             while (true)
             {
-                double Move_X = (Destination_X - Current_X) * 0.15;
-                double Move_Y = (Destination_Y - Current_Y) * 0.15;
+                double Move_X = (destinationpoint.x - currentpoint.x) * 0.15;
+                double Move_Y = (destinationpoint.y - currentpoint.y) * 0.15;
                 int INT_Move_X = (int)Move_X;
                 int INT_Move_Y = (int)Move_Y;
 
@@ -195,16 +205,16 @@ namespace 検証用
                 {
                     INT_Move_Y = 1;
                 }
-                Current_X = Current_X + INT_Move_X;
-                Current_Y = Current_Y + INT_Move_Y;
+                currentpoint.x = currentpoint.x + INT_Move_X;
+                currentpoint.y = currentpoint.y + INT_Move_Y;
 
-                Win32API.SetCursorPos(Current_X, Current_Y);
-                currentX.Text = Current_X.ToString();
-                currentY.Text = Current_Y.ToString();
+                Win32API.SetCursorPos(currentpoint.x, currentpoint.y);
+                currentX.Text = currentpoint.x.ToString();
+                currentY.Text = currentpoint.y.ToString();
 
-                if (Destination_X == Current_X | Destination_Y == Current_Y)
+                if (destinationpoint.x == currentpoint.x | destinationpoint.y == currentpoint.y)
                 {
-                    Win32API.SetCursorPos(Destination_X, Destination_Y);
+                    Win32API.SetCursorPos(destinationpoint.x, destinationpoint.y);
                     break;
                 }
                 System.Threading.Thread.Sleep(20);
@@ -236,10 +246,10 @@ namespace 検証用
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int[] CurrentCoordinate = new int[2];
-            CurrentCoordinate = Win32API.GetCurrentMouseCoordinate();
-            x_coordinate.Text = CurrentCoordinate[0].ToString();
-            y_coordinate.Text = CurrentCoordinate[1].ToString();
+            Win32API.POINT currentpoint = new Win32API.POINT();
+            currentpoint = Win32API.GetCurrentMouseCoordinate();
+            x_coordinate.Text = currentpoint.x.ToString();
+            y_coordinate.Text = currentpoint.y.ToString();
         }
 
         private void Button_Click_MoveSmoothlyButton(object sender, RoutedEventArgs e)
@@ -255,10 +265,14 @@ namespace 検証用
                 // Check that the data is valid before using it
                 if (gazePoint.validity == tobii_validity_t.TOBII_VALIDITY_VALID)
                 {
-                    Debug.WriteLine($"Gaze point: {gazePoint.position.x}, {gazePoint.position.y}");
+                    //Debug.WriteLine($"Gaze point: {gazePoint.position.x}, {gazePoint.position.y}");
+                    
+                    // 画面解像度に合わせた座標に変換し、グローバル変数に記録
                     var w_height = System.Windows.SystemParameters.PrimaryScreenHeight;
                     var w_width = System.Windows.SystemParameters.PrimaryScreenWidth;
                     Win32API.SetCursorPos((int)(w_width * gazePoint.position.x / Dpi_Factor), (int)(w_height * gazePoint.position.y / Dpi_Factor));
+                    gazingpoint.x = (int)(w_width * gazePoint.position.x / Dpi_Factor);
+                    gazingpoint.y = (int)(w_height * gazePoint.position.y / Dpi_Factor);
                 }
             }
 
@@ -321,7 +335,132 @@ namespace 検証用
 
         private void MoveOverLay_Click(object sender, RoutedEventArgs e)
         {
-            overlay.MoveCat();
+            Thickness thickness = new Thickness(100, 100, 0, 0);
+            overlay.MoveCat(thickness);
+        }
+
+        private void DrawCircle_Click(object sender, RoutedEventArgs e)
+        {
+            // マウスの位置に円を描画し続ける
+            // まずメインスレッドをオブジェクトに入れておく
+            var context = SynchronizationContext.Current;
+            void DrawCircleContinuous()
+            {
+                while (true)
+                {
+                    Win32API.POINT currentpoint = new Win32API.POINT();
+                    currentpoint = Win32API.GetCurrentMouseCoordinate();
+
+                    // 描画処理だけメインスレッドで実行させるがここの書き方は正直よくわかってない
+                    context.Post(__ =>
+                    {
+                        int true_x_coordinate = (int)(currentpoint.x * Dpi_Factor);
+                        int true_y_coordinate = (int)(currentpoint.y * Dpi_Factor);
+                        overlay.DrawCircle(true_x_coordinate, true_y_coordinate, GazingCircleAreaWidth_init, GazingCircleAreaHeight_init);
+                    }, null);
+                    Thread.Sleep(10);
+                }
+            }
+            Task task = new Task(DrawCircleContinuous);
+            task.Start();
+        }
+
+        private void DrawCircleAndClick_Click(object sender, RoutedEventArgs e)
+        {
+            // マウスの位置に円を描画し続ける
+            // まずメインスレッドをオブジェクトに入れておく
+            var context = SynchronizationContext.Current;
+
+            void DrawCircleContinuous()
+            {
+                // 座標保存用変数とフラグの宣言
+                Win32API.POINT pastpoint = new Win32API.POINT();
+                pastpoint.x = -100;
+                pastpoint.y = -100;
+                Win32API.POINT currentpoint = new Win32API.POINT();
+                long pasttime = 0;
+                long nowtime = 0;
+                bool IsNotMove = false;
+                bool IsStarted = false;
+                // 時間計測用のストップウォッチ
+                var stopwatch = new System.Diagnostics.Stopwatch();
+
+                while (true)
+                {
+                    currentpoint = Win32API.GetCurrentMouseCoordinate();
+                    //currentpoint = gazingpoint;
+                    // 比較して一定範囲内ならフラグをTrue　範囲外ならFalse
+                    double distance = CalculateDistance(pastpoint, currentpoint) * Dpi_Factor;
+                    Debug.WriteLine(distance);
+                    if (distance < 10)
+                    {
+                        Debug.WriteLine("true");
+                        IsNotMove = true;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("false");
+                        // 注視していない場合は初期値にリセット
+                        IsNotMove = false;
+                        GazingCircleAreaWidth = GazingCircleAreaWidth_init;
+                        GazingCircleAreaHeight = GazingCircleAreaHeight_init;
+                    }
+                    Debug.WriteLine(IsNotMove);
+                    // 描画処理だけメインスレッドで実行させるがここの書き方は正直よくわかってない
+                    context.Post(__ =>
+                    {
+                        int true_x_coordinate = (int)(currentpoint.x * Dpi_Factor);
+                        int true_y_coordinate = (int)(currentpoint.y * Dpi_Factor);
+                        overlay.DrawCircle(true_x_coordinate, true_y_coordinate, GazingCircleAreaWidth, GazingCircleAreaHeight);
+                    }, null);
+
+                    // フラグがTrueならSW始動　Falseなら時間をリセット
+                    if (IsNotMove == true & IsStarted == false)
+                    {
+                        Debug.WriteLine("start");
+                        stopwatch.Start();
+                        pasttime = stopwatch.ElapsedMilliseconds;
+                        IsStarted = true;
+                    }
+                    else if(IsNotMove == false & IsStarted == true)
+                    {
+                        stopwatch.Stop();
+                        stopwatch.Reset();
+                        IsStarted = false;
+                    }
+                    // 経過時間を確認し、NeedTimeToGaze/GazingCircleAreaWidth_initごとに注視円を狭める　規定時間を超えたら左クリックしてリセット
+                    nowtime = stopwatch.ElapsedMilliseconds;
+                    Debug.WriteLine($"nowtime{nowtime}");
+                    Debug.WriteLine($"pasttime{pasttime}");
+                    Debug.WriteLine(nowtime - pasttime);
+                    if ((nowtime - pasttime) > (NeedTimeToGaze / GazingCircleAreaWidth_init))
+                    {
+                        GazingCircleAreaWidth -= 1;
+                        GazingCircleAreaHeight -= 1;
+                        pasttime = nowtime;
+                    }
+
+                    if (GazingCircleAreaWidth < 0)
+                    {
+                        GazingCircleAreaWidth = 0;
+                        GazingCircleAreaHeight = 0;
+                    }
+                    if (stopwatch.ElapsedMilliseconds > NeedTimeToGaze)
+                    {
+                        Debug.WriteLine("finish");
+                        Win32API.MouseLeftClick(currentpoint.x, currentpoint.y, SingleOrDouble);
+                        break;
+                    }
+                    Debug.WriteLine($"Width{GazingCircleAreaWidth}");
+                    // 座標保存
+                    pastpoint = currentpoint;
+                    Thread.Sleep(10);
+                }
+            }
+            Task task = new Task(DrawCircleContinuous);
+            Task task2 = new Task(StreamSample.StreamSampleMain);
+            task.Start();
+            //task2.Start();
         }
     }
 }
--- 検証用/OverLay.xaml.cs
+++ 検証用/OverLay.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.Runtime.InteropServices;
 using System.Diagnostics;
+using System.Windows.Interop;
 
 namespace 検証用
 {
@@ -26,10 +28,65 @@ namespace 検証用
             InitializeComponent();
         }
 
-        public void MoveCat()
+        public class Win32API
         {
-            Cat.VerticalAlignment = 0;
+            // ウインドウにマウスクリックを透過させる用の諸々
+            public const int GWL_EXSTYLE = (-20);
+            public const int WS_EX_TRANSPARENT = 0x00000020;
+
+            [DllImport("user32")]
+            public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+            [DllImport("user32")]
+            public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwLong);
+        }
+
+        public void SetClickTransparent(object sender, RoutedEventArgs e)
+        {
+            // ウィンドウのLoadedイベントに設定し、マウスクリックが背後に透過するように設定するための関数
+            // 拡張ウィンドウスタイルを用いる
+            // OverLayウィンドウのハンドルを取得
+            var handle = new WindowInteropHelper(this).Handle;
+            // 現在の設定の取得
+            int extendStyle = Win32API.GetWindowLong(handle, Win32API.GWL_EXSTYLE);
+            // ビット和で設定の追加
+            extendStyle |= Win32API.WS_EX_TRANSPARENT;
+            Win32API.SetWindowLong(handle, Win32API.GWL_EXSTYLE, extendStyle);
+        }
+
+        public void MoveCat(Thickness thickness)
+        {
+            Cat.Margin = thickness;
             Debug.Write("a");
+        }
+
+        public void DrawCircle(int X, int Y, int WIDTH, int HEIGHT)
+        {
+            // 描画されているものがあればまず消す
+            OverLayCanvas.Children.Clear();
+            // Create a red Ellipse.
+            Ellipse myEllipse = new Ellipse();
+
+            // Create a SolidColorBrush with a red color to fill the
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values.
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(100, 255, 255, 0);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 1;
+            myEllipse.Stroke = Brushes.Black;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = WIDTH;
+            myEllipse.Height = HEIGHT;
+
+            // Add the Ellipse to the Canvas.
+            OverLayCanvas.Children.Add(myEllipse);
+            // 円の半径分ズラした位置に描画
+            Canvas.SetLeft(myEllipse, X-(int)(myEllipse.Width/2));
+            Canvas.SetTop(myEllipse, Y-(int)(myEllipse.Height/2));
         }
     }
 }
